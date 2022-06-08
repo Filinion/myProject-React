60e4f8c3c2034d44702580313841c90a0100a15f
Igor <105514617+Filinion@users.noreply.github.com>
2022-06-08 18:40:56 +0300

    Lesson 30 React

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,24 +1,37 @@
 import classes from "./Dialogs.module.css";
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
+import React from "react";
 
 const Dialogs = (props) => {
   let dialogs = props.state.dialogsData.map((dialog) => (
     <DialogItem name={dialog.name} id={dialog.id} />
   ));
 
+  let sendMessage = React.createRef();
+
   let messages = props.state.messagesData.map((el) => {
     return <Message message={el.messages} id={el.id} />;
   });
+
+  let clickSendMessage = () => {
+    let text = sendMessage.current.value;
+    alert(text);
+  };
 
   return (
     <div className={classes.dialogs}>
       <div className={classes.dialogsItems}>{dialogs}</div>
       <div className={classes.messages}>{messages}</div>
       <div className={classes.textMessage}>
-        <textarea cols={40} rows={4} placeholder="Введите сообщение"></textarea>
+        <textarea
+          ref={sendMessage}
+          cols={40}
+          rows={4}
+          placeholder="Введите сообщение"
+        ></textarea>
         <div className={classes.btnMessage}>
-          <button>Отправить</button>
+          <button onClick={clickSendMessage}>Отправить</button>
         </div>
       </div>
     </div>
